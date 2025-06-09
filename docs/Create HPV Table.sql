02e2ec32d2beedad5039a2c973a69e4cd307d358
Eric Pinto <eric.pinto@nhs.net>
2025-06-09 09:40:48 +0100

    Added Date columns

--- docs/Create HPV Table.sql
+++ docs/Create HPV Table.sql
@@ -1,6 +1,11 @@
-CREATE TABLE [Data_Lab_NCL_Dev].[GrahamR].[hpv_data_test](
+--DROP TABLE [Data_Lab_NCL_Dev].[GrahamR].[Hpv_Data]
+
+CREATE TABLE [Data_Lab_NCL_Dev].[GrahamR].[Hpv_Data](
 Local_Authority VARCHAR(40),
+Year_Group TINYINT,
 Gender VARCHAR(20),
 Number INT,
-Number_Vaccinated INT
+Number_Vaccinated INT,
+Academic_Year_End_Date DATE,
+Extract_Date DATE
 )
